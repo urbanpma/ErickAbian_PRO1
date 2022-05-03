0e03ed5ba6c4ba73db0b99c52ec50708d60af855
urbanpma <urbanpma30@gmail.com>
2022-05-02 21:37:33 -0500

    main

---
+++ app/src/main/java/com/ubnsoftware/encuestasutp/AppActivity.java
@@ -0,0 +1,14 @@
+package com.ubnsoftware.encuestasutp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AppActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app);
+    }
+}
--- app/src/main/java/com/ubnsoftware/encuestasutp/MainActivity.java
+++ app/src/main/java/com/ubnsoftware/encuestasutp/MainActivity.java
@@ -2,7 +2,9 @@ package com.ubnsoftware.encuestasutp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SystemClock.sleep(2500);
+        Intent AppIntent = new Intent(MainActivity.this, AppActivity.class);
+        startActivity(AppIntent);
+        finish();
     }
 }
