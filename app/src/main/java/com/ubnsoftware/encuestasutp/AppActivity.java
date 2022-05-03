a35910350024df9e231abf5ed40377f655e49764
urbanpma <urbanpma30@gmail.com>
2022-05-02 23:20:30 -0500

    main

--- app/src/main/java/com/ubnsoftware/encuestasutp/AppActivity.java
+++ app/src/main/java/com/ubnsoftware/encuestasutp/AppActivity.java
@@ -2,13 +2,36 @@ package com.ubnsoftware.encuestasutp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AppActivity extends AppCompatActivity {
+
+    private Button btnEncuesta, btnSalir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app);
+
+        btnEncuesta = findViewById(R.id.btnEncuesta);
+        btnSalir = findViewById(R.id.btnSalir);
+
+        btnEncuesta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent iEncuesta = new Intent(AppActivity.this, EncuestaActivity.class);
+                startActivity(iEncuesta);
+            }
+        });
+
+        btnSalir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
