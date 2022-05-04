dda20c824ef0438b838e36917b3690d63d3ae18f
urbanpma <urbanpma30@gmail.com>
2022-05-03 22:37:43 -0500

    main

--- app/src/main/java/com/ubnsoftware/encuestasutp/EncuestaActivity.java
+++ app/src/main/java/com/ubnsoftware/encuestasutp/EncuestaActivity.java
@@ -59,5 +59,27 @@ public class EncuestaActivity extends AppCompatActivity {
             public void onNothingSelected(AdapterView<?> parent) {
             }
         });
+
+        Spinner gradoEstudio = (Spinner) findViewById(R.id.spGradoEstudio);
+        ArrayAdapter<CharSequence> adapter2= ArrayAdapter.createFromResource(this,
+                R.array.gradoEstudido, android.R.layout.simple_spinner_dropdown_item);
+
+        gradoEstudio.setAdapter(adapter2);
+
+        gradoEstudio.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+                Object item = parent.getItemAtPosition(pos);
+                if (item.equals("ELIGE UNA OPCIÓN:")){
+                    return;
+                }
+                else{
+                    Toast.makeText(getApplicationContext(), "SELECCIONASTE LA SEDE: " + item,
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
     }
 }
